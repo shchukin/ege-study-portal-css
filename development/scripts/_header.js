881d29c35ef7c599ea0a6a9d867c625149d9cc10
Anton Shchukin <a.a.shchukin@gmail.com>
2022-10-31 22:14:25 +0700

    header links

--- development/scripts/_header.js
+++ development/scripts/_header.js
@@ -5,14 +5,14 @@
 
             /* Закрываем все остальные */
             $('.header__handler--expanded').removeClass('header__handler--expanded');
-            $($('.header__dropdown')).hide();
+            $($('.header__dropdown')).removeClass('header__dropdown--visible');
 
             /* Открываем нужный */
-            $('#' + $(this).data('dropdown')).show();
+            $('#' + $(this).data('dropdown')).addClass('header__dropdown--visible');
             $(this).addClass('header__handler--expanded');
 
         } else {
-            $($('.header__dropdown')).hide();
+            $($('.header__dropdown')).removeClass('header__dropdown--visible');
             $('.header__handler--expanded').removeClass('header__handler--expanded');
         }
     });
@@ -20,7 +20,7 @@
     /* закрываем по клику на Esc */
     $(document).on('keyup', function (event) {
         if (event.keyCode === 27) {
-            $($('.header__dropdown')).hide();
+            $($('.header__dropdown')).removeClass('header__dropdown--visible');
             $('.header__handler--expanded').removeClass('header__handler--expanded');
         }
     });
@@ -28,7 +28,7 @@
     /* закрываем по клику вне дропдауна */
     $(document).on('click', function (event) {
         if (!$(event.target).closest('.header__dropdown, [data-dropdown]').length) {
-            $($('.header__dropdown')).hide();
+            $($('.header__dropdown')).removeClass('header__dropdown--visible');
             $('.header__handler--expanded').removeClass('header__handler--expanded');
         }
     });
