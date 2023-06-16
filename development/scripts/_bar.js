16bc39a01be975936296a2fb0812b14ba722f59d
Anton Shchukin <a.a.shchukin@gmail.com>
2023-06-16 11:11:51 +0700

    mobile bar draft

--- development/scripts/_bar.js
+++ development/scripts/_bar.js
@@ -11,16 +11,16 @@
 
         if ( $(window).scrollTop() < stickPosition ) {
             $bar.removeClass('bar_sticky');
-            $bar.removeClass('bar_static');
+            $bar.removeClass('bar--static');
         }
 
         if ( $(window).scrollTop() >= stickPosition && $(window).scrollTop() < unStickPosition ) {
             $bar.addClass('bar_sticky');
-            $bar.removeClass('bar_static');
+            $bar.removeClass('bar--static');
         }
 
         if ( $(window).scrollTop() >= unStickPosition ) {
-            $bar.addClass('bar_static');
+            $bar.addClass('bar--static');
             $bar.removeClass('bar_sticky');
         }
     }
@@ -31,8 +31,8 @@
         $(window).off('scroll', stickBar);
         $(window).off('resize', stickBar);
         $bar.removeClass('bar_sticky');
-        $bar.addClass('bar_static');
-        $bar.addClass('bar_grounded');
+        $bar.addClass('bar--static');
+        $bar.addClass('bar--grounded');
     }
 
     $('.bar__close').on('click', groundBar);
