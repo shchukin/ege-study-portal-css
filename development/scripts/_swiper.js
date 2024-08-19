2639a8ebef7d38adb4b5184d72d411b1fcd7cf48
Anton Shchukin <a.a.shchukin@gmail.com>
2024-08-19 12:28:38 +0700

    swiper and header

---
+++ development/scripts/_swiper.js
@@ -0,0 +1,87 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    /* Слайдер "Feedback" */
+
+    new Swiper('.swiper--feedback', {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: parseInt(getComputedStyle(document.documentElement).getPropertyValue('--container-padding')),
+        autoHeight: true,
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+        navigation: {
+            prevEl: '#feedback-prev',
+            nextEl: '#feedback-next',
+        },
+
+        breakpoints: {
+            740: {
+                slidesPerView: "auto",
+                spaceBetween: 20,
+                slidesPerGroup: 3,
+                autoHeight: false
+            }
+        }
+    });
+
+
+
+
+    /* Слайдер "Results" */
+
+    new Swiper('.swiper--results', {
+
+        spaceBetween: parseInt(getComputedStyle(document.documentElement).getPropertyValue('--container-padding')),
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+        breakpoints: {
+            740: {
+                loop: true,
+                slidesPerView: "auto",
+                centeredSlides: true,
+                spaceBetween: 20,
+                pagination: false
+            }
+        }
+    });
+
+
+
+
+    /* Слайдер "Blog" */
+
+    new Swiper('.swiper--blog', {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: parseInt(getComputedStyle(document.documentElement).getPropertyValue('--container-padding')),
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+
+        breakpoints: {
+            740: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+                spaceBetween: 20
+            }
+        }
+    });
+
+
+
+});
