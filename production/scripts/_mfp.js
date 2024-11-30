5eaeb98a2a9e91d112c8fe454e3016a4f8860f39
Anton Shchukin <a.a.shchukin@gmail.com>
2024-11-30 11:32:14 +0700

    build

--- production/scripts/_mfp.js
+++ production/scripts/_mfp.js
@@ -1,9 +1,26 @@
-(function($) {
+(function ($) {
 
     $('.mfp-handler').magnificPopup({
         type: 'inline',
         removalDelay: 200,
         showCloseBtn: false
+    });
+
+    $(document).ready(function () {
+        const $poster = $('#poster');
+        if ($poster.length) {
+            setTimeout(function () {
+                $.magnificPopup.open({
+                    items: {
+                        src: '#poster',
+                        type: 'inline'
+                    },
+                    showCloseBtn: false
+                });
+            }, 1000);
+        }
+
+
     });
 
 })(jQuery);
